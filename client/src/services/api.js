53a91aefc5ebf56abfe1b5951e5577be48b0cb7d
R.Durga Bhavani <durgabhavaniragi466@gmail.com>
2026-08-02 01:22:28 +0530

    fix api routes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://fitness-tracker-rc0q.onrender.com/',
+  baseURL: 'https://fitness-tracker-rc0q.onrender.com/api/',
 });
 
 // Request interceptor to attach JWT token
