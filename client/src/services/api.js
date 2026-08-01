3385ce73254ac097455c727ffdcf720bdbe72907
R.Durga Bhavani <durgabhavaniragi466@gmail.com>
2026-08-02 01:01:18 +0530

    update frontend api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fitness-tracker-rc0q.onrender.com/',
 });
 
 // Request interceptor to attach JWT token
